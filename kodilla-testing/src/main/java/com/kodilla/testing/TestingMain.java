ce6944c7e5e1b0579cb1b5cb29e534e565ca210f
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-09-07 17:15:14 +0200

    6.2 Zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        int c;
+        //dodawanie test
+        c = Calculator.add(4,5);
+        if(c == 9) {
+            System.out.println("ok");
+        }else{
+            System.out.println("error");
+        }
+        //odejmowanie test
+        c = Calculator.substract(6,5);
+        if(c == -1) {
+            System.out.println("ok");
+        }else{
+            System.out.println("error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int substract(int a, int b){
+        return a-b;
+    }
+
+}
