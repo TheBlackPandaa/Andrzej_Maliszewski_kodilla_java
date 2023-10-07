842f737ca0351d52b74ca32955c2abdc5f87948a
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-10-07 12:51:25 +0200

    8.3 Zadanie: obsługa wyjątku

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.io;
+
+import com.kodilla.exception.test.ExceptionHandling;
+import com.kodilla.exception.test.SecondChallenge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondChallengeTestSuite {
+    @Test
+    public void testSecondChallenge() {
+        // given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        // when & then
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1, 1.5)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 1)),
+                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1.5, 1))
+        );
+    }
+}
+
+
