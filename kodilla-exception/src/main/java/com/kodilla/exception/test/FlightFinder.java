0b1a7094dd316d2002a4fe98f21d247846e7c302
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-10-30 18:09:19 +0100

    8.4. Obsługiwanie błędów na różnych przypadkach

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightFinder {
-    public void findFlight(Flight flight) throws RouteNotFoundException{
+    public boolean findFlight(Flight flight) throws RouteNotFoundException{
 
         Map <String,Boolean> flights = new HashMap<>();
         flights.put("Warsaw",true);
-        flights.put("Gdansk",true);
+        flights.put("Gdansk",false);
         flights.put("Wroclaw",true);
-        flights.put("Krakow",true);
+        flights.put("Krakow",false);
 
-        if(flights.containsKey(flight.getArrivalAirport()) && flights.containsKey(flight.getDepartureAirport())){
-            System.out.println("viable connection");
+        if(flights.get(flight.getArrivalAirport())){
+            return true;
         }else{
             throw new RouteNotFoundException();
         }
