cea39138ad5cf9f2a9857820b9e350bd2073b1c6
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-11-12 13:29:59 +0100

    14.3 tablica z listami zadań .2

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,28 +1,43 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
 
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
+
     @Bean
+    public Board getBoard(){
+        return new Board(toDoList,inProgressList,doneList);
+    }
+
+    @Bean(name = "toDoList")
     public TaskList getToDoList(){
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "inProgressList")
     public TaskList getInProgressList(){
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "doneList")
     public TaskList getDoneList(){
         return new TaskList();
-    }
-
-    @Bean
-    public Board board(){
-        return new Board(getToDoList(),getInProgressList(),getDoneList());
     }
 }
